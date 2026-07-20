99debf181ac8ba21ca02f8115c2ed8ce1d6a0947
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-07-20 13:51:43 +0200

    chore: Scaffolded from Claude

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import favicon from "$lib/assets/favicon.svg";
+  import "../app.css";
+  import "@digdir/designsystemet-web";
 
   let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
