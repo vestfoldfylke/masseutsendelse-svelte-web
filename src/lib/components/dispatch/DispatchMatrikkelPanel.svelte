7752855b1edb2cbc8fc7558bfd6a29dbe43b3a8c
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-07-22 14:06:24 +0200

    chore: Removed unused type

--- src/lib/components/dispatch/DispatchMatrikkelPanel.svelte
+++ src/lib/components/dispatch/DispatchMatrikkelPanel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { exportOwnersToCsv } from "$lib/dispatch/exportOwners";
-  import type { Dispatch, MatrikkelUnit, Owner } from "$lib/dispatch/types";
+  import type { Dispatch, Owner } from "$lib/dispatch/types";
   import Loading from "../Loading.svelte";
   import DispatchMap from "../Map.svelte";
   import MatrikkelOwnerTable from "../MatrikkelOwnerTable.svelte";
